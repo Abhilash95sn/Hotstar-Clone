9a8c218d3da97e3d5b802124ea793083e6221d76
Abhilash95sn <snabhilash2000@gmail.com>
2024-03-25 16:30:41 +0530

    doggle are complete

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,7 @@
 import HeaderItem from './HeaderItem';
-import logo from './../public/logo-d-plus.webp'
-import React from 'react'
+import logo from '/logo-d-plus.webp'
+import profile from '/png-transparent-head-the-dummy-avatar-man-tie-jacket-user-thumbnail.png'
+import React, { useState } from 'react'
 import {
     AiFillHome,
     AiOutlineSearch,
@@ -9,9 +10,9 @@ import {
     AiOutlinePlus,
 
 } from "react-icons/ai";
-import { RiComputerFill } from "react-icons/ri";
-
+import { BsThreeDotsVertical } from "react-icons/bs";
 const Header = () => {
+    const [toggle, setToggle] = useState(false)
     const menu = [
         {
             name: 'HOME',
@@ -34,16 +35,38 @@ const Header = () => {
             Icon: AiOutlinePlus
         },
     ]
+
     return (
         <>
             <div className='w-full'>
                 <div className=' w-[90%] mx-auto flex '>
                     <div className='w-[50%]'><img src={logo} className='w-[40%] md:w-[115px]   object-cover' /></div>
-                    <div className='w-[70%] flex p-[10px] justify-between mx-5 hover:'>
+
+                    <div className='hidden md:flex p-[10px] justify-between mx-10 '>
                         {menu.map((item) => (
                             <HeaderItem name={item.name} Icon={item.Icon} />
                         ))}
                     </div>
+                    <div className='md:hidden  w-[70%] flex p-[10px] justify-between mx-5 '>
+                        {menu.map((item, index) => index < 3 && (
+                            <HeaderItem name={''} Icon={item.Icon} />
+                        ))}
+                        <div className='md:hidden flex items-center ' onClick={() => setToggle(!toggle)}>
+                            <HeaderItem name={''} Icon={BsThreeDotsVertical} />
+                            {toggle &&
+                                <div className='absolute mt-48 bg-black border-[1px] py-5 gap-5 flra'>
+                                    {menu.map((item, index) => index > 2 && (
+                                        <HeaderItem name={item.name} Icon={item.Icon} />
+                                    ))}
+                                </div>
+                            }
+                        </div>
+                    </div>
+
+                    <div className=' flex items-center w-16 '>
+                        <img src={
+                            profile
+                        } className='rounded-full w-12 hover:w-14 transition duration-500 ease-in-out ' /></div>
 
                 </div>
             </div>
--- src/HeaderItem.jsx
+++ src/HeaderItem.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const HeaderItem = ({ name, Icon }) => {
     return (
-        <div className='text-white flex items-center gap-x-2 hover:text-blue-500 transition duration-500'>
+        <div className='text-white flex items-center gap-x-2 hover:text-blue-500 transition duration-500 hover:underline underline-offset-8  duration-400 px-5 mb-3'>
             <Icon />
-            <h2>{name}</h2>
+            <h2 className=''>{name}</h2>
         </div>
+
     )
 }
 
